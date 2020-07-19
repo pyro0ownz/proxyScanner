277bb78f893aefdbc06137ccaa2aec5f248a0035
Joshua Ragland <40363536+pyro0ownz@users.noreply.github.com>
2020-07-19 15:15:29 -0400

    Add files via upload

---
+++ ProsxyScanner/ProsxyScanner/Main.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace ProsxyScanner
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Found = new System.Windows.Forms.ListBox();
+            this.dec1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.port = new System.Windows.Forms.TextBox();
+            this.dec4 = new System.Windows.Forms.TextBox();
+            this.dec3 = new System.Windows.Forms.TextBox();
+            this.dec2 = new System.Windows.Forms.TextBox();
+            this.Start = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Interval = new System.Windows.Forms.TextBox();
+            this.Sockets = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(401, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.clearToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.fileToolStripMenuItem.Text = "Menu";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.infoToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // infoToolStripMenuItem
+            // 
+            this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+            this.infoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.infoToolStripMenuItem.Text = "Info";
+            // 
+            // Found
+            // 
+            this.Found.BackColor = System.Drawing.SystemColors.MenuText;
+            this.Found.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.Found.FormattingEnabled = true;
+            this.Found.HorizontalScrollbar = true;
+            this.Found.Location = new System.Drawing.Point(104, 41);
+            this.Found.Name = "Found";
+            this.Found.Size = new System.Drawing.Size(186, 95);
+            this.Found.TabIndex = 1;
+            // 
+            // dec1
+            // 
+            this.dec1.Location = new System.Drawing.Point(6, 19);
+            this.dec1.MaxLength = 3;
+            this.dec1.Name = "dec1";
+            this.dec1.Size = new System.Drawing.Size(31, 20);
+            this.dec1.TabIndex = 2;
+            this.dec1.Text = "200";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.groupBox1.Controls.Add(this.port);
+            this.groupBox1.Controls.Add(this.dec4);
+            this.groupBox1.Controls.Add(this.dec3);
+            this.groupBox1.Controls.Add(this.dec2);
+            this.groupBox1.Controls.Add(this.dec1);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.groupBox1.Location = new System.Drawing.Point(88, 144);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(218, 50);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "                 Range                         Port                              " +
+    "    ";
+            // 
+            // port
+            // 
+            this.port.Location = new System.Drawing.Point(162, 19);
+            this.port.MaxLength = 5;
+            this.port.Name = "port";
+            this.port.Size = new System.Drawing.Size(50, 20);
+            this.port.TabIndex = 6;
+            this.port.Text = "1080";
+            // 
+            // dec4
+            // 
+            this.dec4.Location = new System.Drawing.Point(114, 19);
+            this.dec4.MaxLength = 3;
+            this.dec4.Name = "dec4";
+            this.dec4.Size = new System.Drawing.Size(29, 20);
+            this.dec4.TabIndex = 5;
+            this.dec4.Text = "241";
+            // 
+            // dec3
+            // 
+            this.dec3.Location = new System.Drawing.Point(78, 19);
+            this.dec3.MaxLength = 3;
+            this.dec3.Name = "dec3";
+            this.dec3.Size = new System.Drawing.Size(30, 20);
+            this.dec3.TabIndex = 4;
+            this.dec3.Text = "100";
+            // 
+            // dec2
+            // 
+            this.dec2.Location = new System.Drawing.Point(43, 19);
+            this.dec2.MaxLength = 3;
+            this.dec2.Name = "dec2";
+            this.dec2.Size = new System.Drawing.Size(29, 20);
+            this.dec2.TabIndex = 3;
+            this.dec2.Text = "192";
+            // 
+            // Start
+            // 
+            this.Start.BackColor = System.Drawing.Color.Transparent;
+            this.Start.Location = new System.Drawing.Point(88, 243);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.TabIndex = 4;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = false;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(231, 243);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(75, 23);
+            this.Stop.TabIndex = 5;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.CausesValidation = false;
+            this.label1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label1.Location = new System.Drawing.Point(99, 203);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Interval";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label2.Location = new System.Drawing.Point(199, 203);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Sockets";
+            // 
+            // Interval
+            // 
+            this.Interval.Location = new System.Drawing.Point(147, 199);
+            this.Interval.MaxLength = 5;
+            this.Interval.Name = "Interval";
+            this.Interval.Size = new System.Drawing.Size(46, 20);
+            this.Interval.TabIndex = 8;
+            this.Interval.Text = "1000";
+            // 
+            // Sockets
+            // 
+            this.Sockets.Location = new System.Drawing.Point(251, 200);
+            this.Sockets.MaxLength = 3;
+            this.Sockets.Name = "Sockets";
+            this.Sockets.Size = new System.Drawing.Size(39, 20);
+            this.Sockets.TabIndex = 9;
+            this.Sockets.Text = "20";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.progressBar1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.progressBar1.Location = new System.Drawing.Point(27, 281);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(344, 23);
+            this.progressBar1.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label3.Location = new System.Drawing.Point(158, 321);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Count:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label4.Location = new System.Drawing.Point(199, 321);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "0";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(401, 356);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.Sockets);
+            this.Controls.Add(this.Interval);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Found);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Text = "Scanner";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem infoToolStripMenuItem;
+        private System.Windows.Forms.ListBox Found;
+        private System.Windows.Forms.TextBox dec1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox port;
+        private System.Windows.Forms.TextBox dec4;
+        private System.Windows.Forms.TextBox dec3;
+        private System.Windows.Forms.TextBox dec2;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Interval;
+        private System.Windows.Forms.TextBox Sockets;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ProsxyScanner/ProsxyScanner/Main.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+
+
+namespace ProsxyScanner
+{
+    public partial class Main : Form
+    {
+
+        public Main()
+        {
+            InitializeComponent();
+            // Declaration
+            menuStrip1.ForeColor = Color.DimGray;
+
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            this.Stop.Enabled = false;
+            
+        }
+
+        private bool _stop;
+
+        private async void Start_Click(object sender, EventArgs e)
+        {
+            //disable button and boxes plsu stop loop
+            _stop = false;
+            this.dec1.Enabled = false;
+            this.dec2.Enabled = false;
+            this.dec3.Enabled = false;
+            this.dec4.Enabled = false;
+            this.port.Enabled = false;
+            this.Interval.Enabled = false;
+            this.Sockets.Enabled = false;
+            this.Start.Enabled = false;
+            this.Stop.Enabled = true;
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 65025;
+            progressBar1.Step = 1;
+            // end disable 
+
+            //ip stuff 
+            string ip = this.dec1.Text.ToString() + "." + this.dec2.Text.ToString() + "." + this.dec3.Text.ToString() + "." + this.dec4.Text.ToString();
+            int port = int.Parse(this.port.Text);
+            int sockets = int.Parse(Sockets.Text);
+            int time = int.Parse(Interval.Text);
+            int decimal3 = int.Parse(dec3.Text);
+            int decimal4 = int.Parse(dec4.Text);
+
+            //load the amount of sockets to use 
+ 
+            Socket[] connection = new Socket[sockets];
+
+            //while loop scaning 
+            Restart:
+            try
+            {
+                
+                while (decimal3 < 255)
+                {
+
+                    //for loop to control section 
+                    for (int i = 0; decimal4 < 256; i++)
+                    {
+
+                        for (int j = 0; j < sockets && !_stop; j++)
+                        {
+
+                            if (decimal4 < 255)
+                            { //create the socket your going to use in the array
+                                connection[j] = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                                //connect with the socket you made to the port and ip with a wait time for the connection
+                                connection[j].ConnectAsync(ip, port).Wait(time);
+                                //if the connection is connected add and close
+                                await Task.Delay(time);
+                                if (connection[j].Connected)
+                                {
+                                    Found.Items.Add(ip + ":" + port);
+                                    label4.Text = Found.Items.Count.ToString();
+                                    connection[j].Close();
+                                    progressBar1.PerformStep();
+                                    decimal4++;
+                                    this.dec4.Text = decimal4.ToString();
+                                    ip = this.dec1.Text.ToString() + "." + this.dec2.Text.ToString() + "." + decimal3.ToString() + "." + decimal4.ToString();
+                                }
+                                else if (!connection[j].Connected)
+                                {
+                                    //increment and then set the ip to new value
+                                    progressBar1.PerformStep();
+                                    decimal4++;
+                                    this.dec4.Text = decimal4.ToString();
+                                    ip = this.dec1.Text.ToString() + "." + this.dec2.Text.ToString() + "." + decimal3.ToString() + "." + decimal4.ToString();
+
+                                    //close the connection afterwords 
+
+                                    connection[j].Close();
+                                }
+                            }
+                            //if last decimal is 255 reset it to zero add it to text box then increment the 3rd decimal
+                            if (decimal4 == 255)
+                            {
+                                decimal4 = 0;
+                                this.dec4.Text = decimal4.ToString();
+                                decimal3++;
+                                dec3.Text = decimal3.ToString();
+
+                            }
+
+                        }
+                        await Task.Delay(1000);
+                    }
+
+                    if (decimal3 == 255 & decimal4 == 255)
+                    {
+                        MessageBox.Show("Done");
+                        break;
+
+                    }
+                }
+            }
+
+            catch
+            {
+                decimal4++;
+                this.dec4.Text = decimal4.ToString();
+                ip = this.dec1.Text.ToString() + "." + this.dec2.Text.ToString() + "." + decimal3.ToString() + "." + decimal4.ToString();
+                goto Restart;
+            }
+
+
+        }
+   
+
+
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            //enable all boxes and buttons disabled by start button
+            this.dec1.Enabled = true;
+            this.dec2.Enabled = true;
+            this.dec3.Enabled = true;
+            this.dec4.Enabled = true;
+            this.port.Enabled = true;
+            this.Interval.Enabled = true;
+            this.Sockets.Enabled = true;
+            this.Start.Enabled = true;
+            this.Stop.Enabled = false;
+            _stop = true;
+
+
+            //end section 
+
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Found.Items.Clear();
+            label4.Text = "0";
+        }
+    }
+
+}
